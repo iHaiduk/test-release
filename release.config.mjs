3abd4b6400f5f0d3ca27f8e669799e904a58dc99
Ihor Haiduk <cookkycok@gmail.com>
2025-01-23 22:49:02 +0200

    feat: config release

--- release.config.mjs
+++ release.config.mjs
@@ -7,9 +7,42 @@ const preset = 'conventionalcommits'
  * @type {import('semantic-release').Options}
  **/
 export default {
+  branches: [
+    "+([0-9])?(.{+([0-9]),x}).x",
+    "{main,master}",
+    "next",
+    "next-major",
+    {
+      name: "beta",
+      prerelease: true
+    },
+    {
+      name: "alpha",
+      prerelease: true
+    }
+  ],
   "plugins": [
+    "semantic-release-pinst",
     ['@semantic-release/commit-analyzer', {preset}],
-    ['@semantic-release/release-notes-generator', {preset}],
+    ['@semantic-release/release-notes-generator', {
+      preset,
+      writerOpts: {
+        types: [
+          { type: 'feat', section: 'Features' },
+          { type: 'feature', section: 'Features' },
+          { type: 'fix', section: 'Bug Fixes' },
+          { type: 'perf', section: 'Performance Improvements' },
+          { type: 'revert', section: 'Reverts' },
+          { type: 'docs', section: 'Documentation' },
+          { type: 'style', section: 'Styles', hidden: true },
+          { type: 'chore', section: 'Miscellaneous Chores', hidden: true },
+          { type: 'refactor', section: 'Code Refactoring' },
+          { type: 'test', section: 'Tests', hidden: true },
+          { type: 'build', section: 'Build System', hidden: true },
+          { type: 'ci', section: 'Continuous Integration', hidden: true }
+        ]
+      }
+    }],
     [
       "@semantic-release/changelog",
       {
@@ -21,5 +54,9 @@ export default {
       "assets": ["docs", "package.json"],
       "message": "chore(release): ${nextRelease.version} :tada: [skip ci]\n\n${nextRelease.notes}"
     }]
+  ],
+  releaseRules: [
+    {type: "docs", release: "patch"},
+    {type: "refactor", release: "patch"}
   ]
 }
