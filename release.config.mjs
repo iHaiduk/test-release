20802106430772e385f23564f45d3e5f073b5ecd
Ihor Haiduk <cookkycok@gmail.com>
2025-01-23 22:45:28 +0200

    perf: update release file

--- release.config.mjs
+++ release.config.mjs
@@ -1,17 +1,25 @@
+// @ts-check
+/* eslint-disable no-template-curly-in-string */
+
+const preset = 'conventionalcommits'
+
+/**
+ * @type {import('semantic-release').Options}
+ **/
 export default {
   "plugins": [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
+    ['@semantic-release/commit-analyzer', {preset}],
+    ['@semantic-release/release-notes-generator', {preset}],
     [
       "@semantic-release/changelog",
       {
         "changelogFile": "docs/CHANGELOG.md"
       }
     ],
+    "@semantic-release/github",
     ["@semantic-release/git", {
       "assets": ["docs", "package.json"],
-      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-    }],
-    "@semantic-release/github"
+      "message": "chore(release): ${nextRelease.version} :tada: [skip ci]\n\n${nextRelease.notes}"
+    }]
   ]
 }
