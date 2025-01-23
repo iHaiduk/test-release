73281308e4b4dba32654ac05250cb7dbe9eaa076
Ihor Haiduk <cookkycok@gmail.com>
2025-01-23 22:50:54 +0200

    feat: config release

--- release.config.mjs
+++ release.config.mjs
@@ -24,25 +24,25 @@ export default {
   "plugins": [
     "semantic-release-pinst",
     ['@semantic-release/commit-analyzer', {preset}],
-    ['@semantic-release/release-notes-generator', {
-      preset,
-      writerOpts: {
-        types: [
-          { type: 'feat', section: 'Features' },
-          { type: 'feature', section: 'Features' },
-          { type: 'fix', section: 'Bug Fixes' },
-          { type: 'perf', section: 'Performance Improvements' },
-          { type: 'revert', section: 'Reverts' },
-          { type: 'docs', section: 'Documentation' },
-          { type: 'style', section: 'Styles', hidden: true },
-          { type: 'chore', section: 'Miscellaneous Chores', hidden: true },
-          { type: 'refactor', section: 'Code Refactoring' },
-          { type: 'test', section: 'Tests', hidden: true },
-          { type: 'build', section: 'Build System', hidden: true },
-          { type: 'ci', section: 'Continuous Integration', hidden: true }
-        ]
-      }
-    }],
+    // ['@semantic-release/release-notes-generator', {
+    //   preset,
+    //   writerOpts: {
+    //     types: [
+    //       { type: 'feat', section: 'Features' },
+    //       { type: 'feature', section: 'Features' },
+    //       { type: 'fix', section: 'Bug Fixes' },
+    //       { type: 'perf', section: 'Performance Improvements' },
+    //       { type: 'revert', section: 'Reverts' },
+    //       { type: 'docs', section: 'Documentation' },
+    //       { type: 'style', section: 'Styles', hidden: true },
+    //       { type: 'chore', section: 'Miscellaneous Chores', hidden: true },
+    //       { type: 'refactor', section: 'Code Refactoring' },
+    //       { type: 'test', section: 'Tests', hidden: true },
+    //       { type: 'build', section: 'Build System', hidden: true },
+    //       { type: 'ci', section: 'Continuous Integration', hidden: true }
+    //     ]
+    //   }
+    // }],
     [
       "@semantic-release/changelog",
       {
