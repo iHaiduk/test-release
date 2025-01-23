0746e61610bd76448ebbc65b366a04afbc1450e1
Ihor Haiduk <cookkycok@gmail.com>
2025-01-23 22:19:40 +0200

    feat: add @semantic-release/github

--- release.config.mjs
+++ release.config.mjs
@@ -11,6 +11,7 @@ export default {
     ["@semantic-release/git", {
       "assets": ["docs", "package.json"],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-    }]
+    }],
+    "@semantic-release/github"
   ]
 }
