3c9ed6ada1b0011fedf752b4f7c14852e9cefe6e
Ihor Haiduk <cookkycok@gmail.com>
2025-01-23 21:58:29 +0200

    feat: added new semantic

---
+++ release.config.mjs
@@ -0,0 +1,16 @@
+export default {
+  "plugins": [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        "changelogFile": "docs/CHANGELOG.md"
+      }
+    ],
+    ["@semantic-release/git", {
+      "assets": ["docs", "package.json"],
+      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+    }]
+  ]
+}
